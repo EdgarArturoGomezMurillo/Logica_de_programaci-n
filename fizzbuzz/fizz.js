7e9f1dfea13e131402396f9a40e08fc5fe218256
arturo <arturogomezmurillo@gmail.com>
2021-12-22 18:24:44 -0500

    ejercicios fizzbuzz

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -1,4 +1,4 @@
-// ejercisio para que escriba del 1 al 100 y fizz si es divicible por 3 coloca fizz si es divisible por 5 coloca buzz y de los dos fizzbuzz
+// ejercicio para que escriba del 1 al 100 y fizz si es divicible por 3 coloca fizz si es divisible por 5 coloca buzz y de los dos fizzbuzz
 var numeros = 100;
 for (var i = 1; i <= 100; i++) {
     if (esDivisible(i, 3)) {
